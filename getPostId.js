9235e7488e908f8cc156203dc6235cdda328963d
menglingzhe <menglingzhe@meituan.com>
2023-04-21 02:52:40 +0800

    delete useless code的

--- getPostId.js
+++ getPostId.js
@@ -43,42 +43,7 @@ async function getPostIdsByDate(uid, targetDateStr, callback) {
     }
     console.log('true', createdAt)
 
-    // 评论card处理
-    // await cards.forEach(async card => {
 
-    //   if (card.card_type === 9) {
-    //     const mblog = card.mblog;
-    //     const id = mblog.id; //主贴id
-    //     const blogger = mblog.user.screen_name //博主
-    //     mBlogger = blogger
-
-    //     ids.push(id);
-    //     //从此开始写评论爬取
-    //     let totalMark = await getPostInfo.getPostInfo(id, (err, id) => { //评论
-    //       console.log('enter mark success')
-    //       if (err) {
-    //         console.error('markErr', err);
-    //         return;
-    //       }
-    //     })
-
-    //     totalMark = tools.refreshWord(totalMark)
-    //     if (totalMark.length > 10) {
-    //       fileData.data.push({
-    //         'postId:': mblog.id, //主贴id
-    //         'blogger:': mblog.user.screen_name, //博主
-    //         'articelTime:': new Date(mblog.created_at.replace(/-/g, '/')), //发布时间
-    //         'commentsCount:': mblog.comments_count, //评
-    //         'repostsCount:': mblog.reposts_count, //转
-    //         'likeCount:': mblog.attitudes_count, //赞
-    //         'picCount:': mblog.pic_num, //图
-    //         'articel:': tools.refreshWord(mblog.text), //主贴
-    //         'totalMark:': totalMark
-    //       })
-    //     }
-
-    //   }
-    // });
     let weiBoRow = await getList.getList(cards)
     if (weiBoRow != null) {
       fileData.data.push(...weiBoRow)
