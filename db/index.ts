d572f5cfd42abaa4c8d75170b1f76b72e49d552a
menglingzhe <menglingzhe@meituan.com>
2023-04-24 23:21:05 +0800

    项目结构优化

--- db/index.ts
+++ db/index.ts
@@ -6,7 +6,7 @@ export const db = mysql.createPool({
   host: "localhost", // 数据库的 IP 地址
   // host: '127.0.0.1', // 数据库的 IP 地址
   user: "root", // 登录数据库的账号
-
+  password: "admin123", // 登录数据库的密码
   // password: "123456", // 登录数据库的密码
   database: "weibomark", // 指定要操作哪个数据库
   port: 3306,
