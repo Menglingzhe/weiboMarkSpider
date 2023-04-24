c97e1bc9fc076dacbf0d6d08a75222d658d93612
menglingzhe <menglingzhe@meituan.com>
2023-04-24 23:24:34 +0800

    优化项目结构

---
+++ dist/db/index.js
@@ -0,0 +1,38 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.db = void 0;
+// import mysql from "mysql";
+var mysql = __importStar(require("mysql"));
+exports.db = mysql.createPool({
+    //link数据库
+    host: "localhost",
+    // host: '127.0.0.1', // 数据库的 IP 地址
+    user: "root",
+    password: "admin123",
+    // password: "123456", // 登录数据库的密码
+    database: "weibomark",
+    port: 3306,
+});
---
+++ dist/db/sql.js
@@ -0,0 +1,72 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.saveToSql = void 0;
+// 导入数据库操作模块
+var index_1 = require("./index");
+function saveToSql(Items) {
+    console.log("开始insert SQL操作");
+    var sqlStr = "insert into markdata(postid,blogger,articeltime,commentscount,repostscount,likecount,piccount,articel,totalmark,bloggeruid,interactive) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+    if (Items.length == 0) {
+        console.log('评论数为0');
+        return;
+    }
+    for (var i = 0; i < Items.length; i++) {
+        // console.log('Item:',Items[i])
+        var modSqlParams = [
+            Items[i].postId,
+            Items[i].blogger,
+            Items[i].articelTime,
+            Items[i].commentsCount,
+            Items[i].repostsCount,
+            Items[i].likeCount,
+            Items[i].picCount,
+            Items[i].articel,
+            Items[i].totalMark,
+            Items[i].bloggerUid,
+            Items[i].interactive,
+        ];
+        index_1.db.query(sqlStr, modSqlParams, function (err, results) {
+            if (err)
+                return console.log(err.message);
+        });
+    }
+    console.log("结束insert SQL操作");
+}
+exports.saveToSql = saveToSql;
+// export function testSaveToSql() {
+//   console.log("开始SQL操作");
+//   let sqlStr =
+//     "insert into markdata(postid,blogger,articeltime,commentscount,repostscount,likecount,piccount,articel,totalmark) VALUES (?,?,?,?,?,?,?,?,?)";
+//   const Item: any = {
+//     postId: "4729625417094330",
+//     blogger: "武汉发布",
+//     articelTime: "2022-01-25T12:33:54.000Z",
+//     commentsCount: 3,
+//     repostsCount: 2,
+//     likeCount: 8,
+//     picCount: 0,
+//     articel:
+//       "生活服务武汉将于日后半夜至日凌晨迎雪是三年来最大降雪月日记者从省气象部门获悉根据最新气象资料分析预计月日全省自西向东将有大范围强降雪天气过程其中武汉的降雪降雨日后半夜或日凌晨到来此次降雪程度为最近三年最大武汉将于日后半夜至日凌晨迎雪是三年来最大降雪",
+//     totalMark: "放大招了真下雪不闹眼子了",
+//     bloggerUid: "6582815618",
+//   };
+//   if ((Item.length = 0)) return;
+//     const modSqlParams = [
+//       Item.postId,
+//       Item.blogger,
+//       Item.articelTime,
+//       Item.commentsCount,
+//       Item.repostsCount,
+//       Item.likeCount,
+//       Item.picCount,
+//       Item.articel,
+//       Item.totalMark,
+//       Item.bloggerUid
+//   ];
+//   console.log('插入测试')
+//     db.query(sqlStr, modSqlParams, (err, results) => {
+//       if (err) return console.log(err.message);
+//     });
+//   console.log("结束SQL操作");
+// }
+// testSaveToSql();
